a93bf4906b090ecc6def4311ff6b0f91de87bf3d
Mihirshah-ms <2025.mihirn@isu.ac.in>
2026-02-01 19:44:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import os
+import psycopg2
 
 st.title("🏁 F1 Winner AI")
-st.write("If you can see this page, your app is live 🚀")
+
+db_url = os.getenv("DATABASE_URL")
+
+if not db_url:
+    st.error("Database not connected")
+else:
+    try:
+        conn = psycopg2.connect(db_url)
+        st.success("✅ Database connected successfully!")
+        conn.close()
+    except Exception as e:
+        st.error("❌ Database connection failed")
